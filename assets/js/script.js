16fa19adaec38e03eb15f8f6a3b080ad37414fb5
Dedes <federico.lepore73@gmail.com>
2026-01-13 22:06:08 +0100

    script.js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// bottoni
+const saldiBtn = document.getElementById("saldiSec");
+const welcomeBtn = document.getElementById("welcomeSec");
+// section
+const saldiSection = document.getElementById("saldi");
+const welcomeSection = document.getElementById("welcome-summer");
+
