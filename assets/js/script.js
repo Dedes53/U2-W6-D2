c11e1fa58a531c869b25a4617c75891fe3f61088
Dedes <federico.lepore73@gmail.com>
2026-01-13 22:24:37 +0100

    section button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,28 @@ const welcomeBtn = document.getElementById("welcomeSec");
 // section
 const saldiSection = document.getElementById("saldi");
 const welcomeSection = document.getElementById("welcome-summer");
+// testi
+const showText = "Mostra sezione"
+const hideText = "Nascondi Sezione"
 
+
+
+saldiBtn.addEventListener("click", () => {
+    if (saldiSection.style.display !== "none") {
+        saldiSection.style.display = "none";
+        saldiBtn.textContent = showText;
+    } else {
+        saldiSection.style.display = "flex";
+        saldiBtn.textcontent = hideText;
+    }
+})
+
+welcomeBtn.addEventListener("click", () => {
+    if (welcomeSection.style.display !== "none") {
+        welcomeSection.style.display = "none";
+        welcomeBtn.textContent = showText;
+    } else {
+        welcomeSection.style.display = "flex";
+        welcomeBtn.textcontent = hideText;
+    }
+})
